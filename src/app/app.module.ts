b5895e3a3688a81783d55cee719a36b9943917e1
Leah Sandacz <sandas@outlook.com>
2018-09-27 18:43:55 -0700

    three.js scene with pane for editor controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThreeSceneComponent } from './three-scene/three-scene.component';
+import { EditorPaneComponent } from './editor-pane/editor-pane.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreeSceneComponent,
+    EditorPaneComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/editor-pane/editor-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-pane',
+  templateUrl: './editor-pane.component.html',
+  styleUrls: ['./editor-pane.component.css']
+})
+export class EditorPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
